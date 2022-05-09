45a52459559be97903f33f1503f9ed64e4ce6fd0
Vinicius Gonçalves <artefinal@Carbografites-iMac.local>
2022-05-09 16:59:06 -0300

    comesando a implantar o JSON

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const express= require('express')
+
+const app = express()
+
+app.listen('3000') 
+
+//app.route('/').get((req,res)=> res.send("oi"))
+
+//middleware
+app.use(express.json())
+
+
+//app.route('/').post( (req,res) => console.log(req.body) )
+
+//DELETE
+//app.route('/:identificador').delete( (req,res) => {
+//    res.send(req.params.identificador)
+//})
+
+app.route('/').get( (req,res) => res.send("oi"))
+
