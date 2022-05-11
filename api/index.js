e0f0eabfb97f1cef03272bc75c2798484c29312c
Otavio Costa <otavio2000@gmail.com>
2022-05-11 12:24:08 -0300

    adicionar cabecalhos de CORS

--- api/index.js
+++ api/index.js
@@ -9,6 +9,18 @@ app.listen('3000')
 //middleware
 app.use(express.json())
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  // Pass to next layer of middleware
+  next();
+});
+
 app.route('/').post( (req,res) => console.log(req.body) )
 
 //DELETE
